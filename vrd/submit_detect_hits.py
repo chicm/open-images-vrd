7d887ac56e84eec1a645a62761392f01f918f758
chicm <idv2005@gmail.com>
2019-09-07 13:10:41 +0000

    updates

---
+++ vrd/submit_detect_hits.py
@@ -0,0 +1,100 @@
+import os
+import numpy as np
+import pandas as pd
+import pickle
+import argparse
+from tqdm import tqdm
+from vrd.utils import get_image_size
+import  settings
+
+classes = [
+    '/m/04yx4,/m/01226z',
+    '/m/04yx4,/m/0wdt60w',
+    '/m/01bl7v,/m/01226z',
+    '/m/05r655,/m/01226z',
+    '/m/03bt1vf,/m/01226z',
+    '/m/04yx4,/m/05ctyq',
+    '/m/03bt1vf,/m/05ctyq',
+    '/m/05r655,/m/05ctyq',
+    '/m/01226z',
+    '/m/0wdt60w',
+    '/m/05ctyq'
+    #'/m/04yx4',
+    #'/m/01bl7v',
+    #'/m/05r655',
+    #'/m/03bt1vf'
+    ]
+stoi = { classes[i]: i for i in range(len(classes)) }
+
+num_classes = 11
+
+def get_preds(raw_pred, threshold=0):
+    res = {
+        'labels': [],
+        'scores': [],
+        'bboxes': []
+    }
+    for i, p in enumerate(raw_pred[:num_classes]):
+        if len(p) > 0:
+            for e in p:
+                if e[4] > threshold:
+                    res['labels'].append(i)
+                    res['scores'].append(e[4])
+                    res['bboxes'].append(e[:4])
+    res['labels'] = np.array(res['labels'])
+    res['scores'] = np.array(res['scores'])
+    res['bboxes'] = np.array(res['bboxes'])
+    return res
+
+def get_pred_str(pred, w, h, classes):
+    res = []
+    for label, score, bbox in zip(pred['labels'], pred['scores'], pred['bboxes']):
+        res.append(classes[label])
+        res.append('{:.7f}'.format(score))
+        res.append('{:.7f}'.format(bbox[0]/w))
+        res.append('{:.7f}'.format(bbox[1]/h))
+        res.append('{:.7f}'.format(bbox[2]/w))
+        res.append('{:.7f}'.format(bbox[3]/h))
+    res = [str(x) for x in res]
+    return ' '.join(res)
+
+def create_submit(args, img_list_file='VRD_sample_submission.csv', img_dir=settings.TEST_IMG_DIR):
+    with open(args.pred_file, 'rb') as f:
+        print('loading: ', args.pred_file)
+        preds = pickle.load(f)
+
+    df_test = pd.read_csv(os.path.join(settings.DATA_DIR, img_list_file))
+    print('getting image sizes')
+    df_test['h'] = df_test.ImageId.map(lambda x: get_image_size(os.path.join(img_dir, '{}.jpg'.format(x)))[1])
+    df_test['w'] = df_test.ImageId.map(lambda x: get_image_size(os.path.join(img_dir, '{}.jpg'.format(x)))[0])
+    print(df_test.head())
+
+    final_preds = []
+
+    for p in tqdm(preds, total=len(preds)):
+        final_preds.append(get_preds(p, args.th))
+    total_objs = 0
+    for p in final_preds:
+        total_objs += len(p['labels'])
+    print('total predicted objects:', total_objs)
+
+    #classes, _ = get_top_classes()
+    pred_strs = []
+    for i, p in tqdm(enumerate(final_preds), total=len(final_preds)):
+        h = df_test.iloc[i].h
+        w = df_test.iloc[i].w
+        pred_strs.append(get_pred_str(p, w, h, classes))
+    df_test['PredictionString'] = pred_strs
+    print(df_test.head())
+    df_test.to_csv(args.out, index=False, columns=['ImageId', 'PredictionString'])
+    print('done')
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='create submission from pred file')
+    parser.add_argument('--pred_file', type=str, required=True)
+    parser.add_argument('--out', type=str, required=True)
+    parser.add_argument('--th', type=float, default=0.)
+    args = parser.parse_args()
+
+    create_submit(args)
---
+++ vrd/submit_tuple_hits_detect.py
@@ -0,0 +1,209 @@
+import os
+import argparse
+import pandas as pd
+import numpy as np
+import time
+import math
+from multiprocessing import Pool
+from catboost import CatBoostClassifier
+from utils import get_iou
+from train_hits import add_features, parallel_apply
+
+classes_1 = set([
+    '/m/04yx4,/m/01226z',
+    '/m/04yx4,/m/0wdt60w',
+    '/m/01bl7v,/m/01226z',
+    '/m/05r655,/m/01226z',
+    '/m/03bt1vf,/m/01226z',
+    '/m/04yx4,/m/05ctyq',
+    '/m/03bt1vf,/m/05ctyq',
+    '/m/05r655,/m/05ctyq'
+    ])
+
+classes_2 = set(['/m/01226z', '/m/0wdt60w', '/m/05ctyq'])
+
+model = None
+
+def get_sort_score(row):
+    return math.sqrt(row.confidence1 * row.confidence2) * row.coef
+
+def get_pred_str(row):
+    conf = round(row.sort_score,6)
+    rel = [conf, row.LabelName1, row.XMin1, row.YMin1, row.XMax1, row.YMax1,
+               row.LabelName2, row.XMin2, row.YMin2, row.XMax2, row.YMax2, row.RelationshipLabel]
+    rel = [str(x) for x in rel]
+    return ' '.join(rel)
+
+def fast_get_prediction_string(test_row):
+    dets = test_row.dets
+
+    dets_c1 = [x for x in dets if x[0] in classes_1]
+    dets_c2 = [x for x in dets if x[0] in classes_2]
+
+    if len(dets_c1) < 1 or len(dets_c2) < 1:
+        return ''
+
+    cur_test_dets = []
+    #dets = sorted(dets, key=lambda x: x[1], reverse=True)  #[:35]
+    #for i in range(len(dets)):
+    #    for j in range(len(dets)):
+    for det1 in dets_c1:
+        for det2 in dets_c2:
+            c1, c2 = det1[0].split(',')
+            if c1 != det2[0]:
+            #if c2 == det2[0]:
+            #det1, det2 = dets[i], dets[j]
+            #if i != j and ','.join([det1[0], det2[0]]) in classes:
+            #if i != j and det1[0] in classes_1 and det2[0] in classes_2:
+                cur_test_dets.append({
+                    #'ImageID': test_row.ImageId,
+                    'LabelName1': c1,
+                    'LabelName2': det2[0],
+                    'XMin1': float(det1[2]),
+                    'XMax1': float(det1[4]),
+                    'YMin1': float(det1[3]),
+                    'YMax1': float(det1[5]),
+                    'XMin2': float(det2[2]),
+                    'XMax2': float(det2[4]),
+                    'YMin2': float(det2[3]),
+                    'YMax2': float(det2[5]),
+                    'confidence1': float(det1[1]),
+                    'confidence2': float(det2[1]),
+                })
+    
+    if len(cur_test_dets) < 1:
+        return ''
+
+    cur_df = pd.DataFrame(cur_test_dets)
+
+    cur_df = add_features(cur_df)
+    cur_x_test = cur_df.drop(['confidence1', 'confidence2'], axis=1)
+    
+    pred_score = model.predict_proba(cur_x_test)
+    pred_rel = model.predict(cur_x_test)
+
+    cur_x_test['coef'] = pred_score[:, 1]
+    cur_x_test['confidence1'] = cur_df['confidence1']
+    cur_x_test['confidence2'] = cur_df['confidence2']
+    cur_x_test['RelationshipLabel'] = pd.Series(pred_rel).map(lambda x: 'hits' if x == 1 else 'none')
+    
+    cur_x_test['sort_score'] = cur_x_test.apply(lambda row: get_sort_score(row), axis=1)
+    
+    cur_x_test = cur_x_test.loc[cur_x_test.RelationshipLabel != 'none'].copy()
+
+    cur_x_test = cur_x_test.nlargest(30, 'sort_score').copy()
+    #cur_x_test.sort_values(by='sort_score', axis=0, ascending=False, inplace=False, kind='quicksort')
+    #cur_x_test = cur_x_test[:200].copy()
+
+    if len(cur_x_test) < 1:
+        return ''
+
+    cur_x_test['PredictionString'] = cur_x_test.apply(lambda row: get_pred_str(row), axis=1)
+    #print(cur_x_test.head())
+    
+    return ' '.join(cur_x_test.PredictionString.values)
+
+tuple_classes = set(classes_1) | set(classes_2)
+
+def get_det(pred_str):
+    dets = []
+    det = []
+    for i, e in enumerate(pred_str.split(' ')):
+        if i % 6 == 0:
+            det = []
+        det.append(e)
+        if (i+1) % 6 == 0:
+            if det[0] in set(tuple_classes): #and float(det[1]) > 0.1:
+                dets.append(det)
+            
+    return dets
+
+def add_pred_string(df):
+    df['PredictionString'] = df.apply(lambda row: fast_get_prediction_string(row), axis=1)
+    return df
+
+def submit(args):
+    global model
+    #model = CatBoostEnsembleModel()
+    model = CatBoostClassifier() #task_type="GPU")
+    print('loading {}...'.format(args.model_file))
+    model.load_model(args.model_file)
+    #print(dir(model))
+    print(model.classes_)
+    #print(model.feature_importances_)
+    print(model._tree_count)
+    print(model.learning_rate_)
+
+    #exit(0)
+
+    print('loading {}...'.format(args.detect_pred_file))
+    df_det = pd.read_csv(args.detect_pred_file)
+    df_det['dets'] = df_det.PredictionString.map(lambda x: get_det(str(x)))
+    print('detected objs:', df_det.dets.map(lambda x: len(x)).sum())
+
+    print('predicting...')
+    df_sub = df_det.copy()
+    df_sub.PredictionString = ''
+
+    bg = time.time()
+    df_sub = parallel_apply(df_sub, add_pred_string)
+    df_sub.to_csv(args.out, columns=['ImageId', 'PredictionString'], index=False)
+
+    print('Done, total time:', time.time() - bg)
+
+
+def submit_val_preds(args):
+    def get_pred_dets(pred_str):
+        if len(pred_str) < 1:
+            return []
+        dets = []
+        det = []
+        for i, e in enumerate(pred_str.split(' ')):
+            if i % 12 == 0:
+                det = []
+            det.append(e)
+            if (i+1) % 12 == 0:
+                dets.append(det)
+                
+        return dets
+
+    submit(args)
+
+    df_sub = pd.read_csv(args.out)
+    df_sub.PredictionString = df_sub.PredictionString.fillna('')
+    res = []
+    for i, row in df_sub.iterrows():
+        objs = get_pred_dets(row.PredictionString)
+        for o in objs:
+            det_obj = {
+                'ImageID': row.ImageId,
+                'LabelName1': o[1],
+                'XMin1': o[2],
+                'YMin1': o[3],
+                'XMax1': o[4],
+                'YMax1': o[5],
+                'LabelName2': o[6],
+                'XMin2': o[7],
+                'YMin2': o[8],
+                'XMax2': o[9],
+                'YMax2': o[10],
+                'RelationshipLabel': o[11],
+                'Score': o[0]
+            }
+            res.append(det_obj)
+
+    df_val_sub = pd.DataFrame(res)
+    df_val_sub.to_csv(args.out, index=False)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='create submission from detect pred file')
+    parser.add_argument('--detect_pred_file', type=str, required=True)
+    parser.add_argument('--model_file', type=str, required=True)
+    parser.add_argument('--out', type=str, required=True)
+    parser.add_argument('--val', action='store_true')
+    args = parser.parse_args()
+
+    if args.val:
+        submit_val_preds(args)
+    else:
+        submit(args)
